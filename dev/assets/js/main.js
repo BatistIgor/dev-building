ca15b983ed5b42319414409eaa196074fd830b0c
BatistIgot <igorbatist089@gmail.com>
2024-09-21 12:04:50 +0300

    Added adaptive

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,2 +1,27 @@
-const box = document.querySelector('.box')
-const box2 = document.querySelector('.box2')
+const adress = document.querySelector('#adress')
+const floor = document.querySelector('#floor')
+const flat = document.querySelector('#flat')
+const buildPaths = document.querySelectorAll('.build-path')
+
+console.log(adress,floor,flat,buildPaths)
+
+buildPaths.forEach(build => {
+   build.addEventListener('mouseover', ()=> {
+    const dataAdress = build.getAttribute('data-adress')
+    const dataFloor = build.getAttribute('data-floor-quantity')
+    const dataFlat = build.getAttribute('data-flat-quantity')
+
+    adress.innerHTML = dataAdress 
+    floor.innerHTML = dataFloor 
+    flat.innerHTML = dataFlat 
+   })
+})
+
+
+const button = document.querySelector('.hamburger')
+const menu = document.querySelector('.header__navigation')
+
+button.addEventListener('click', ()=> {
+    button.classList.toggle('is-active');
+    menu.classList.toggle('header__navigation-active');
+})
